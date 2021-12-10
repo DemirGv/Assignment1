3287bfabffc8391cf4c8e83fa8b38b909ea0f734
DemirGv <gvargis.demir@gmail.com>
2021-12-10 22:35:58 +0100

    Finished assignment1, Calculator.java

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 ### Calculator 
 
-- create a console-based calculator. 
-- handles basic operations such as addition, subtraction, multiplication and division. 
-- The result of each operation also needs to be presented in a consistent way.
-- ~~Application need to be a Maven Project that runs with Java 8.~~
-- Application need to be able to perform mathematical operations addition, subtraction, multiplication and division.
-- The program should not stop running until the User choose to end it by terminating a loop.
-
-### Optional 
-- Addition and subtraction should be able to handle any amount of numbers to add or subtract. 
-- Handle Exceptions that may happen. In particular when user enters input. 
-- Each mathematical operation should be tested using Junit.  
+handles basic operations 
+- addition
+- subtraction
+- multiplication
+- division
--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -1,4 +1,44 @@
 package se.lexicon;
+import java.util.Scanner;
 
+/**
+ * handles basic operations such as addition,
+ * subtraction, multiplication and division.
+ * The result of each operation is presented in a consistent way.
+ * The program don't stop running until the User choose to end it
+ * by terminating the loop.
+ */
 public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to calculator, write the first number");
+        int number1 = scanner.nextInt();
+        System.out.println("Write one of the four operations +,-,*,/");
+        String operation = scanner.next();
+        System.out.println("Write the second number");
+        int number2 = scanner.nextInt();
+
+        switch (operation) {
+
+            case "-":
+                System.out.println(number1 - number2);
+                break;
+            case "/":
+                System.out.println(number1 / number2);
+                break;
+            case "+":
+                System.out.println(number1 + number2);
+                break;
+            case "*":
+                System.out.println(number1 * number2);
+                break;
+            default:
+                System.out.println("try again");
+        }
+
+    }
+
 }
